041bc8cf8bdce4e99c5189e9159b10ae00ee58ba
kazes <florian.boudot@gmail.com>
2016-02-03 14:11:53 +0100

    carte couche bâtie : ajout de précisions sur la datation

--- map-gl.php
+++ map-gl.php
@@ -104,13 +104,40 @@ global $ti_option;
 
                             <h3>Que reste-t-il de cette époque dans la rue ?</h3>
 
+
                             <p>
-                                source : <a href="http://www.apur.org/article/donnees-disponibles-open-data" target="_blank">APUR, emprise batie</a> + filtre sur datation
+                                Filtre sur datation provenant des données de l'<a href="http://www.apur.org/article/donnees-disponibles-open-data" target="_blank">APUR, emprise batie</a>.
                             </p>
+
 
                             <!-- THE MAP -->
                             <div id="map"></div>
                             <nav id="menu"></nav>
+
+                            <p>
+                                Il s'agit d'une très bonne base pour commencer à répertorier les batiments restants de l'époque mais il faut prendre en considération que ces données peuvent ne pas être à jour ou comporter des erreurs. Voici un extrait de la documentation de ces données :
+                            </p>
+
+                            <p>
+                                "Pour l'information de datation, celle-ci peut également provenir de plusieurs sources :
+                            </p>
+                            <ul>
+                                <li>
+                                    Plan de datation de Paris (avant 1800 jusqu'à 1940) réalisée par François Loyer en 1970. Ce plan, originalement au format papier et éditée à deux échelles (1/10000eme et 1/2000eme) est basé sur le plan parcellaire de l'époque et renseigne sur l'ancienneté et la qualité architecturale des bâtiments sur rue datant d'avant 1940 (et visible en 1970). Inexploitable en tant que telle sur la plate-forme SIG, la carte a été numérisée et l'information de datation reportée sur le SIG. 74 000 bâtiments datant d'avant 1940 ont en effet été datés par un processus de classification automatique permettant de transposer l'information de datation des bâtiments de la carte " Loyer " sur les emprises vectorielles représentant le bâti parisien. Ces bâtiments (sous leur forme vectorielle en SIG) sont donc datés à quelques erreurs informatiques près (entre 5 et 10 % de non-reconnaissance des légendes). Des corrections ont donc du être opérées pour régler ces erreurs de classifications; les vérifications se font faites par enquête terrain.
+                                </li>
+                                <li>
+                                    BD MAJICII de la DGFiP: Cette source indique l'âge du bâti, informations collectées lors de la saisie de déclarations diverses. Concernant la date de construction, les chiffres ne sont toutefois pas disponibles pour l'ensemble des 35 000 bâtiments datant d'après 1940. Cette information (pas toujours renseignée et parfois de façon erronée) est donnée par la DGFiP au niveau d'une parcelle. Un indicateur a donc été créé à la parcelle concernant le bâtiment le plus ancien et le plus récent. L'exactitude de cette date est parfois sujette à caution, d'autant que l'année de construction peut être modifiée dès lors qu'un permis de construire est autorisé sur la parcelle. L'enquête terrain a complété cette exploitation.
+                                </li>
+                                <li>
+                                    Dates des Permis de Construire après 1990 : Les années de déclaration et d'obtention des permis de construire furent communiquées tardivement et localement mais ont permis dans certains cas d'affiner la datation.
+                                </li>
+                                <li>
+                                    Enquête terrain : Le travail d'enquête dans les 6 000 rues de Paris a consisté à expertiser les façades et l'ensemble des corps de bâtiment apparents sur rue afin de leur attribuer une des 6 périodes (post 1940).
+                                </li>
+                                <li>
+                                    Orthophotoplans successives de 2008, 2012, 2013. Thermographie aérienne de (2009)"
+                                </li>
+                            </ul>
 
                             <p>
                                 todo : à croiser avec le plan Alpage Vasserot
